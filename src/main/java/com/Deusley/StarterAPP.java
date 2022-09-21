37dce22a16eb3bc5822683ca15e7c2242f22ea0d
Deuslleyw <94429495+Deuslleyw@users.noreply.github.com>
2022-09-21 18:30:36 -0300

    Start-a-project

---
+++ src/main/java/com/Deusley/StarterAPP.java
@@ -0,0 +1,13 @@
+package com.Deusley;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarterAPP {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarterAPP.class, args);
+	}
+
+}
